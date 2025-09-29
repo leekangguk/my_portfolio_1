261423a07ad8ef108531a5a85a24004fc8d05acf
leekangguk <133728485+leekangguk@users.noreply.github.com>
2025-09-29 19:03:26 +0900

    Add files via upload
    Team_Project_01

---
+++ eunchol_my_02/js/main.js
@@ -0,0 +1,8 @@
+const video1_url="https://www.youtube.com/embed/HC9SJwi_fRk?start=253&end=283&autoplay=1&mute=1&si=bTaiAyODJg4U8opZ"
+const video2_url="https://www.youtube.com/embed/IzXbqedRUzQ?si=w_xbWHRHlsT7P2Ae&amp;start=2&end=32&autoplay=1&mute=1"
+const video3_url="https://www.youtube.com/embed/9khmkc4z3mY?si=wVpdfVmLVEQ_5qqL&amp;start=25&end=55&autoplay=1&mute=1"
+const video4_url="https://www.youtube.com/embed/AboUG-eYFUI?si=kESCSYIi6C24EVAo&amp;start=63&end=93&autoplay=1&mute=1"
+
+function reloadMyIframe(url) {
+ document.getElementById('video1').src=url;
+}
